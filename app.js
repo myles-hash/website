9d2cfca954722583e0b004e69b32d29d231f18da
Myles Billignton <mylesbillington@gmail.com>
2023-12-01 11:28:51 +0000

    add audios and basic js for audios

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 document.getElementById('playSound').addEventListener('click', function() {
     var audio = document.getElementById('myAudio');
     audio.play();
+    event.preventDefault();
+  });
+  document.getElementById('playSound2').addEventListener('click', function() {
+    var audio = document.getElementById('myAudio2');
+    audio.play();
+    event.preventDefault();
+  });
+  document.getElementById('playSound3').addEventListener('click', function() {
+    var audio = document.getElementById('myAudio3');
+    audio.play();
+    event.preventDefault();
   });
 
  
