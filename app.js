eb2c3983544707c264e46a3b72e6cfe060c335d5
Myles Billignton <mylesbillington@gmail.com>
2023-12-01 14:02:28 +0000

    try adding sound to h2 through js

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-document.getElementById('playSound').addEventListener('click', function() {
-    var audio = document.getElementById('myAudio');
+function addClickHandler(buttonId, audioId) {
+  document.getElementById(buttonId).addEventListener('click', function(event) {
+    let audio = document.getElementById(audioId);
     audio.play();
     event.preventDefault();
   });
-  document.getElementById('playSound2').addEventListener('click', function() {
-    var audio = document.getElementById('myAudio2');
-    audio.play();
-    event.preventDefault();
-  });
-  document.getElementById('playSound3').addEventListener('click', function() {
-    var audio = document.getElementById('myAudio3');
-    audio.play();
-    event.preventDefault();
-  });
+}
+
+addClickHandler('playSound', 'myAudio');
+addClickHandler('playSound2', 'myAudio2');
+addClickHandler('playSound3', 'myAudio3');
+
+const h2Element = document.getElementsById('names');
+const audioElement = document.getElementById('glitter');
+
+  function playSound() {
+    audioElement.play();
+  }
+
+  h2Element.addEventListener('mouseover', playSound);
+
+  
 
  
